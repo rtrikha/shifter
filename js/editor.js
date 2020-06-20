ff5d5c450a39729754cb4918085fc65001b765f2
rtikha <raunakstrikha@gmail.com>
2020-06-20 12:18:31 +0530

    adding nodes bifercation

--- js/editor.js
+++ js/editor.js
@@ -1,43 +1,49 @@
-function update()
-{
+function update() {
 	var idoc = document.getElementById('iframe').contentWindow.document;
-
 	idoc.open();
 	idoc.write(editor.getValue());
 	idoc.close();
 }
 
-function setupEditor()
-{
-  window.editor = ace.edit("editor");
-  editor.setTheme("ace/theme/nord_dark");
-  editor.getSession().setMode("ace/mode/html");
-  editor.setValue(`<html>
-<head>
-</head>
+const parent = 'parent';
+const child = 'child';
 
-<body>
-</body>
+function addParent() {
+	editor.session.insert(editor.getCursorPosition(), `<div class="${parent}"></div>`);
+	beautify();
+}
 
-</html>`,1); //1 = moves cursor to end
+function addChild() {
+	editor.session.insert(editor.getCursorPosition(), `<div class="${child}"></div>`);
+	beautify();
+}
 
-  editor.getSession().on('change', function() {
-    update();
-  });
+function setupEditor() {
+	window.editor = ace.edit('editor');
+	editor.setTheme('ace/theme/monokai');
+	editor.getSession().setMode('ace/mode/html');
+	editor.setValue(``, 1); //1 = moves cursor to end
 
-  editor.focus();
-  
-  
-  editor.setOptions({
-    fontSize: "12pt",
-    showLineNumbers: true,
-    showGutter: false,
-    vScrollBarAlwaysVisible:true,
-    enableBasicAutocompletion: false, enableLiveAutocompletion: false
-  });
+	editor.getSession().on('change', function () {
+		update();
+	});
 
-  editor.setShowPrintMargin(false);
-  editor.setBehavioursEnabled(false);
+	editor.focus();
+
+	editor.setOptions({
+		fontSize: '12pt',
+		showLineNumbers: true,
+		showGutter: false,
+	});
+
+	editor.setShowPrintMargin(false);
+	editor.setBehavioursEnabled(false);
+}
+
+function beautify() {
+	var val = editor.session.getValue();
+	val = html_beautify(val);
+	editor.session.setValue(val);
 }
 
 setupEditor();
